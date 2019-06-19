7b0efff08cd5255d41f551cfd868107037b1db39
Stephen Banbury <sbanbury@gmail.com>
2019-06-19 21:01:51 +0100

    Install Angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; // material animations
+import { MatButtonModule, MatCheckboxModule } from '@angular/material'; // for example
+
+// see https://material.angular.io/guide/getting-started
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
